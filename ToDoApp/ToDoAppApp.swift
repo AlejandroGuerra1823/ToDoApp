70e79c3a8fde784e8f80b8c70ba9de9012436ed8
Alejandro Guerra <guerramanuel299@gmail.com>
2024-09-10 21:13:30 -0500

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Alejandro Guerra on 10/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
